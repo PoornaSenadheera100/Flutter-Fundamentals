bd713c06a9efdaa537cafef8d3de34a64954ce15
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-06-24 11:14:22 +0530

    Added Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: Container(),
-    );
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primarySwatch: Colors.deepOrange,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Container"),
+          ),
+          body: Container(
+            color: Colors.indigo,
+            child: const Text("Hello World"),
+          ),
+        ));
   }
 }
