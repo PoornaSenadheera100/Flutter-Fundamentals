7b16d8b016f8d3829ae1707e2d0db9b6dd83b0c4
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-06-28 23:27:40 +0530

    Day 1 + Day 2

---
+++ lib/screens/menu_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+  const MenuScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        leading: const Icon(
+          Icons.home,
+          color: Colors.amber,
+        ),
+        title: const Text(
+          "Heading",
+          style: TextStyle(color: Colors.amber),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.amber,
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+                color: Colors.amber,
+              ))
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Welcome to My App",
+              style: TextStyle(fontSize: 20.0),
+            ),
+            ElevatedButton(onPressed: () {}, child: Text("ON")),
+          ],
+        ),
+      ),
+    );
+  }
+}
