e3cf69a15bc989c1ea9308696243458ac6c4c0cb
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-06-29 13:11:54 +0530

    Added const

--- lib/screens/custom_app_bar_screen.dart
+++ lib/screens/custom_app_bar_screen.dart
@@ -7,17 +7,17 @@ class CustomAppBarScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: IconThemeData(color: Colors.pink),
+        iconTheme: const IconThemeData(color: Colors.pink),
         leading: IconButton(onPressed: (){
-          Navigator.of(context).pop(CustomAppBarScreen());
-        }, icon: Icon(Icons.arrow_back_sharp),),
-        title: Text(
+          Navigator.of(context).pop(const CustomAppBarScreen());
+        }, icon: const Icon(Icons.arrow_back_sharp),),
+        title: const Text(
           "Custom App Bar",
           style: TextStyle(color: Colors.pink),
         ),
         actions: [
-          IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-          IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search)),
+          IconButton(onPressed: () {}, icon: const Icon(Icons.more_vert)),
         ],
         flexibleSpace: Image.asset(
           "assets/app_bar_background.png",
