55f2f8725e5dee8070815382c157cf6a57cd721e
PoornaSenadheera100 <poornasenadheera100@gmail.com>
2023-07-06 14:58:33 +0530

    Implemented flutter forms

--- lib/screens/flutter_forms_screen1.dart
+++ lib/screens/flutter_forms_screen1.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fundamentals/models/FormUser.dart';
+import 'package:flutter_fundamentals/screens/flutter_forms_screen2.dart';
 
 class FlutterFormsScreen extends StatefulWidget {
   const FlutterFormsScreen({super.key});
@@ -13,12 +15,48 @@ class _FlutterFormsScreenState extends State<FlutterFormsScreen> {
   String _phone = '';
   String _password = '';
 
-  GlobalKey _formKey = GlobalKey();
-  TextEditingController _emailController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _emailController = TextEditingController();
 
-  String? _validateName(String text){
-    if(text == ''){
+  String? _validateName(String text) {
+    if (text == '') {
       return "Name is required!";
+    }
+    return null;
+  }
+
+  String? _validateEmail(String text) {
+    if (text == '') {
+      return "Email is required!";
+    } else if (!text.contains("@")) {
+      return "Email must have '@' sign";
+    }
+    return null;
+  }
+
+  String? _validateAddress(String text) {
+    if (text == '') {
+      return "Address is required!";
+    }
+    return null;
+  }
+
+  String? _validatePhone(String text) {
+    if (text == '') {
+      return "Phone is required!";
+    } else if (!text.startsWith("0")) {
+      return "Phone number should start with 0";
+    } else if (text.length < 10) {
+      return "Invalid phone number!";
+    }
+    return null;
+  }
+
+  String? _validatePassword(String text) {
+    if (text == '') {
+      return "Password is required!";
+    } else if (text.length < 8) {
+      return "Password should have at least 8 characters";
     }
     return null;
   }
@@ -27,43 +65,128 @@ class _FlutterFormsScreenState extends State<FlutterFormsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Flutter Forms App"),
+        title: const Text("Flutter Forms App"),
       ),
-      body: Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            Icon(Icons.person),
-            TextFormField(
-              validator: (text){
-                return _validateName(text!);
-              },
-              decoration: InputDecoration(hintText: "Name"),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                const Icon(
+                  Icons.person,
+                  size: 200.0,
+                ),
+                TextFormField(
+                  textCapitalization: TextCapitalization.words,
+                  autofocus: true,
+                  cursorColor: Colors.red,
+                  maxLength: 50,
+                  onSaved: (text) {
+                    _name = text!;
+                  },
+                  validator: (text) {
+                    return _validateName(text!);
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Name",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(bottom: 7.0),
+                      child: Icon(Icons.contact_mail_outlined),
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  maxLength: 50,
+                  keyboardType: TextInputType.emailAddress,
+                  controller: _emailController,
+                  validator: (text) {
+                    return _validateEmail(text!);
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Email",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(bottom: 7.0),
+                      child: Icon(Icons.alternate_email),
+                    ),
+                  ),
+                ),
+                TextField(
+                  style: const TextStyle(color: Colors.green),
+                  enabled: false,
+                  maxLines: 2,
+                  controller: _emailController,
+                  decoration: const InputDecoration(
+                      prefixText: "Your username will be ",
+                      border: InputBorder.none),
+                ),
+                TextFormField(
+                  maxLines: 3,
+                  maxLength: 100,
+                  onSaved: (text) {
+                    _address = text!;
+                  },
+                  validator: (text) {
+                    return _validateAddress(text!);
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Address",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(bottom: 38.0),
+                      child: Icon(Icons.home),
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  maxLength: 10,
+                  keyboardType: TextInputType.phone,
+                  onSaved: (text) {
+                    _phone = text!;
+                  },
+                  validator: (text) {
+                    return _validatePhone(text!);
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Phone",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(bottom: 7.0),
+                      child: Icon(Icons.phone),
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  maxLength: 16,
+                  obscureText: true,
+                  onSaved: (text) {
+                    _password = text!;
+                  },
+                  validator: (text) {
+                    return _validatePassword(text!);
+                  },
+                  decoration: const InputDecoration(
+                    hintText: "Password",
+                    prefixIcon: Padding(
+                      padding: EdgeInsets.only(bottom: 7.0),
+                      child: Icon(Icons.password),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        _formKey.currentState?.save();
+                        FormUser user = FormUser(_name, _emailController.text,
+                            _address, _phone, _password);
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                FlutterFormsScreen2(user)));
+                      }
+                    },
+                    child: const Text("Submit")),
+              ],
             ),
-            TextFormField(
-              controller: _emailController,
-              decoration: InputDecoration(hintText: "Email"),
-            ),
-            TextField(
-              enabled: false,
-              controller: _emailController,
-              decoration: InputDecoration(
-                prefixText: "Your username will be ",
-                  hintText: "Please enter an email.", border: InputBorder.none),
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Address"),
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Phone"),
-            ),
-            TextFormField(
-              decoration: InputDecoration(hintText: "Password"),
-            ),
-            ElevatedButton(onPressed: () {
-
-            }, child: Text("Submit")),
-          ],
+          ),
         ),
       ),
     );
